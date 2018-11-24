a8751fa21d8a86c3feb9ccb8546a15f3ebab67e7
guoweikuang <673411814@qq.com>
2018-11-24 09:19:46 -0500

    添加项目文件以及测试文件

---
+++ project/authentication.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+def login(username, password):
+    try:
+        user_file = open('/etc/users.txt')
+        user_buf = user_file.read()
+        users = [line.split('|') for line in user_buf.split('\n')]
+        if [username, password] in users:
+            return True
+        else:
+            return False
+    except Exception as e:
+        print("can't authentication you")
+        return False
+
+
+def logout():
+    print("这一行不会被测试用例覆盖")
+    print("这一行也不会被测试用例覆盖")
+
+
